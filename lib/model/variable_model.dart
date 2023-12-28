ef19eb79ae45463f6d9e836a99ae6e2004d9b342
PatelVed671 <118421398+PatelVed671@users.noreply.github.com>
2023-12-28 12:18:25 +0530

    Personal Details Form Added...

--- lib/model/variable_model.dart
+++ lib/model/variable_model.dart
@@ -2,4 +2,14 @@ import 'package:flutter/cupertino.dart';
 
 class VariableModal {
   static TextEditingController name = TextEditingController();
+  static TextEditingController email = TextEditingController();
+  static TextEditingController number = TextEditingController();
+  static TextEditingController address = TextEditingController();
+  static TextEditingController dob = TextEditingController();
+  static TextEditingController nationality = TextEditingController();
+  static TextEditingController study = TextEditingController();
+  static TextEditingController jobTitle = TextEditingController();
+  static TextEditingController experience = TextEditingController();
+  static TextEditingController skill = TextEditingController();
+  static TextEditingController project = TextEditingController();
 }
--- lib/views/screens/home_page.dart
+++ lib/views/screens/home_page.dart
@@ -12,22 +12,22 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Filed Details"),
+        backgroundColor: Colors.redAccent.shade400,
       ),
       body: Padding(
-        padding: EdgeInsets.all(18),
+        padding: const EdgeInsets.all(18),
         child: Form(
           key: formKey,
           autovalidateMode: AutovalidateMode.onUserInteraction,
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
                 "Personal Details",
                 style: GoogleFonts.jetBrainsMono(),
               ),
-              SizedBox(height: s.height * 0.01),
               const Divider(),
-              SizedBox(height: s.height * 0.01),
+              // Name
               TextFormField(
                 validator: (value) {
                   if (value!.isEmpty) {
@@ -44,8 +44,103 @@ class HomePage extends StatelessWidget {
                   focusColor: Colors.white,
                   border: const OutlineInputBorder(),
                   labelText: "Name",
-                  fillColor: Colors.grey.shade700,
-                  prefixIcon: const Icon(Icons.person),
+                  filled: true,
+                  fillColor: Colors.grey.shade100,
+                  prefixIcon: const Icon(Icons.perm_identity),
+                ),
+                textInputAction: TextInputAction.next,
+              ),
+              // Email
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Enter The Valid Email";
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (newValue) {
+                  VariableModal.email.text = newValue!;
+                },
+                controller: VariableModal.email,
+                decoration: InputDecoration(
+                  focusColor: Colors.white,
+                  border: const OutlineInputBorder(),
+                  labelText: "Email",
+                  filled: true,
+                  fillColor: Colors.grey.shade100,
+                  prefixIcon: const Icon(Icons.email_outlined),
+                ),
+                textInputAction: TextInputAction.next,
+              ),
+              // Number
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Enter The Valid Number";
+                  } else {
+                    return null;
+                  }
+                },
+                maxLength: 10,
+                onSaved: (newValue) {
+                  VariableModal.number.text = newValue!;
+                },
+                controller: VariableModal.number,
+                decoration: InputDecoration(
+                  focusColor: Colors.white,
+                  border: const OutlineInputBorder(),
+                  labelText: "Number",
+                  filled: true,
+                  fillColor: Colors.grey.shade100,
+                  prefixIcon: const Icon(Icons.phone_android_outlined),
+                ),
+                keyboardType: TextInputType.number,
+                textInputAction: TextInputAction.next,
+              ),
+              // Address
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Enter The Valid Address";
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (newValue) {
+                  VariableModal.address.text = newValue!;
+                },
+                controller: VariableModal.address,
+                decoration: InputDecoration(
+                  focusColor: Colors.white,
+                  border: const OutlineInputBorder(),
+                  labelText: "Address",
+                  filled: true,
+                  fillColor: Colors.grey.shade100,
+                  prefixIcon: const Icon(Icons.add_location_alt_outlined),
+                ),
+                textInputAction: TextInputAction.next,
+              ),
+              // DOB
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Enter The Valid Date";
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (newValue) {
+                  VariableModal.dob.text = newValue!;
+                },
+                controller: VariableModal.dob,
+                decoration: InputDecoration(
+                  focusColor: Colors.white,
+                  border: const OutlineInputBorder(),
+                  labelText: "DOB",
+                  filled: true,
+                  fillColor: Colors.grey.shade100,
+                  prefixIcon: const Icon(Icons.date_range),
                 ),
                 textInputAction: TextInputAction.next,
               ),
