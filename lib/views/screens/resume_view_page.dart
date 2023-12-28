18d4ad6ef79e41ec60ff7baad67f0877cbe16676
ved_patel_28 <130833918+Vedpatel28@users.noreply.github.com>
2023-12-28 13:54:54 +0530

    PDF Creating Start...

--- lib/views/screens/resume_view_page.dart
+++ lib/views/screens/resume_view_page.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ResumePreviewPage extends StatelessWidget {
   const ResumePreviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Size s = MediaQuery.of(context).size;
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+        ),
+        title: Text(
+          "Resume",
+          style: GoogleFonts.jetBrainsMono(
+            textStyle: TextStyle(
+              color: Colors.white,
+              fontSize: s.height * 0.03,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        backgroundColor: Colors.redAccent.shade400,
+      ),
 
     );
   }
